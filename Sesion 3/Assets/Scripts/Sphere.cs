3eb3711b91e87795407d0d7f7f57cf49f4db60e5
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-10-09 15:37:10 +0200

    Ejercicio 3 Sesion 3

---
+++ Sesion 3/Assets/Scripts/Sphere.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Sphere : MonoBehaviour
+{
+    [SerializeField] float radius, time, vel;
+
+    Vector3 dst;
+
+    private void Awake()
+    {
+        StartCoroutine(Move());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = (dst - transform.position).normalized;
+        transform.Translate(dir * vel);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(Vector3.zero, radius);
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            dst = Random.insideUnitSphere * radius;
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
