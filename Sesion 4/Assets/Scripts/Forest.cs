5b242a7f62f3f7abe130f9aa2262c97c57c3e820
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-10-22 19:50:35 +0200

    Ejercicio 3 Sesion 4

---
+++ Sesion 4/Assets/Scripts/Forest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Forest : MonoBehaviour
+{
+    [SerializeField][Range(0, 1)] float IBirds;
+    [SerializeField] float minTime;
+    [SerializeField] float maxTime;
+    [SerializeField] float radius;
+
+    [SerializeField] AudioClip forest_loop;
+    [SerializeField] AudioClip[] birds;
+
+    AudioSource forest_loop_source;
+    AudioSource[] birds_source;
+
+    private void Awake()
+    {
+        forest_loop_source = gameObject.AddComponent<AudioSource>();
+        forest_loop_source.clip = forest_loop;
+        forest_loop_source.volume = IBirds;
+        forest_loop_source.playOnAwake = true;
+        forest_loop_source.loop = true;
+        forest_loop_source.Play();
+
+        birds_source = new AudioSource[birds.Length];
+        for (int i = 0; i < birds.Length; i++)
+        {
+            GameObject go = new GameObject($"Chatter: {i}");
+
+            birds_source[i] = go.AddComponent<AudioSource>();
+            birds_source[i].transform.SetParent(transform);
+            birds_source[i].clip = birds[i];
+            birds_source[i].volume = IBirds;
+            birds_source[i].playOnAwake = false;
+        }
+
+        StartCoroutine(PlayBirdsEveryTime());
+    }
+
+    IEnumerator PlayBirdsEveryTime()
+    {
+        float time = Random.Range(minTime, maxTime);
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+
+            foreach (var bird
+                in birds_source)
+            {
+                if (!bird.isPlaying && Random.Range(0, 1) <= IBirds)
+                {
+                    bird.gameObject.transform.position = Random.insideUnitCircle * (radius / Mathf.Max(IBirds, 0.01f));
+                    bird.volume = IBirds;
+                    bird.Play();
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        forest_loop_source.volume = IBirds;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Sesion 4/Assets/Scripts/Storm.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Storm : MonoBehaviour
+{
+    [SerializeField][Range(0, 1)] float IStorm;
+    [SerializeField] float minTime;
+    [SerializeField] float maxTime;
+    [SerializeField] float radius;
+
+    [SerializeField] AudioClip[] rain;
+    [SerializeField] AudioClip[] wind;
+    [SerializeField] AudioClip[] thunderstorm;
+    [SerializeField] AudioClip[] drop;
+
+    AudioSource[] rain_source;
+    AudioSource[] wind_source;
+    AudioSource[] thunderstorm_source;
+    AudioSource[] drop_source;
+
+    private void Awake()
+    {
+        rain_source = new AudioSource[rain.Length];
+        for (int i = 0; i < rain.Length; i++)
+        {
+            float lambda = 1.0f / rain.Length;
+            GameObject go = new GameObject($"Chatter: {i}");
+
+            rain_source[i] = go.AddComponent<AudioSource>();
+            rain_source[i].transform.SetParent(transform);
+            rain_source[i].clip = rain[i];
+            rain_source[i].volume = Mathf.InverseLerp(lambda * i, lambda * (i + 1), Mathf.Min(IStorm * (lambda * (i + 1)), lambda * (i + 1)));
+            rain_source[i].playOnAwake = true;
+        }
+
+        wind_source = new AudioSource[wind.Length];
+        for (int i = 0; i < wind.Length; i++)
+        {
+            float lambda = 1.0f / wind.Length;
+            GameObject go = new GameObject($"Chatter: {i}");
+
+            wind_source[i] = go.AddComponent<AudioSource>();
+            wind_source[i].transform.SetParent(transform);
+            wind_source[i].clip = wind[i];
+            wind_source[i].volume = Mathf.InverseLerp(lambda * i, lambda * (i + 1), Mathf.Min(IStorm * (lambda * (i + 1)), lambda * (i + 1)));
+            wind_source[i].playOnAwake = true;
+        }
+
+        thunderstorm_source = new AudioSource[thunderstorm.Length];
+        for (int i = 0; i < thunderstorm.Length; i++)
+        {
+            GameObject go = new GameObject($"Chatter: {i}");
+
+            thunderstorm_source[i] = go.AddComponent<AudioSource>();
+            thunderstorm_source[i].transform.SetParent(transform);
+            thunderstorm_source[i].clip = thunderstorm[i];
+            thunderstorm_source[i].volume = IStorm;
+            thunderstorm_source[i].playOnAwake = false;
+        }
+
+        drop_source = new AudioSource[drop.Length];
+        for (int i = 0; i < drop.Length; i++)
+        {
+            GameObject go = new GameObject($"Chatter: {i}");
+
+            drop_source[i] = go.AddComponent<AudioSource>();
+            drop_source[i].transform.SetParent(transform);
+            drop_source[i].clip = drop[i];
+            drop_source[i].volume = IStorm;
+            drop_source[i].playOnAwake = false;
+        }
+
+        StartCoroutine(PlayStormEveryTime());
+    }
+
+    IEnumerator PlayStormEveryTime()
+    {
+        float time = Random.Range(minTime, maxTime);
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+
+            for (int i = 0; i < rain_source.Length; i++)
+            {
+                float lambda = 1.0f / rain_source.Length;
+                rain_source[i].volume = Mathf.InverseLerp(lambda * i, lambda * (i + 1),
+                                            Mathf.Min(IStorm * (lambda * (i + 1)), lambda * (i + 1)));
+            }
+
+            for (int i = 0; i < wind_source.Length; i++)
+            {
+                float lambda = 1.0f / wind_source.Length;
+                wind_source[i].volume = Mathf.InverseLerp(lambda * i, lambda * (i + 1),
+                                            Mathf.Min(IStorm * (lambda * (i + 1)), lambda * (i + 1)));
+            }
+
+            foreach (var thunderstorm in thunderstorm_source)
+            {
+                if (!thunderstorm.isPlaying && Random.Range(0, 1) <= IStorm)
+                {
+                    thunderstorm.gameObject.transform.position = Random.insideUnitCircle * radius;
+                    thunderstorm.volume = IStorm;
+                    thunderstorm.Play();
+                }
+            }
+
+            foreach (var drop in drop_source)
+            {
+                if (!drop.isPlaying && Random.Range(0, 1) <= IStorm)
+                {
+                    drop.gameObject.transform.position = Random.insideUnitCircle * radius;
+                    drop.volume = IStorm;
+                    drop.Play();
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
