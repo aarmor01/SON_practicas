01d42089368398f8d35577b67d3f9be6dc38e012
Rioni <nrosa01@ucm.es>
2022-12-03 18:12:12 +0100

    Añadido navmesh

---
+++ Zona_Costera/Assets/Scripts/MoveCharactersAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveCharactersAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField]Transform[] wayPoints;
+
+    private int RandomIndex => Random.Range(0, wayPoints.Length);
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = wayPoints[RandomIndex].position;
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance <= 0.1f)
+            agent.destination = wayPoints[RandomIndex].position;
+    }
+}
