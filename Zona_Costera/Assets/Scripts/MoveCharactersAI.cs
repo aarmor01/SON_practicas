52159262e495dff41fa686718898c22de5c43ba8
Rioni <nrosa01@ucm.es>
2022-12-03 19:19:47 +0100

    Añadido navmesh link (falta corregir)

--- Zona_Costera/Assets/Scripts/MoveCharactersAI.cs
+++ Zona_Costera/Assets/Scripts/MoveCharactersAI.cs
@@ -6,10 +6,10 @@ using UnityEngine.AI;
 public class MoveCharactersAI : MonoBehaviour
 {
     NavMeshAgent agent;
-    [SerializeField] Transform[] wayPoints;
+    [SerializeField] List<Transform> wayPoints;
     int currentIndex = 0;
 
-    private int RandomIndex => Random.Range(0, wayPoints.Length);
+    private int RandomIndex => Random.Range(0, wayPoints.Count);
 
     private void Awake()
     {
@@ -18,14 +18,36 @@ public class MoveCharactersAI : MonoBehaviour
         agent.destination = wayPoints[currentIndex].position;
     }
 
+    bool reached()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    // Done
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     void Update()
     {
-        if (agent.remainingDistance <= 0.1f)
+        int attemps = 1000;
+        if (reached())
         {
             int aux;
             do
             {
                 aux = RandomIndex;
+                if (attemps <= 0)
+                    break;
+
+                attemps--;
             } while (aux == currentIndex);
             currentIndex = aux;
             agent.destination = wayPoints[currentIndex].position;
