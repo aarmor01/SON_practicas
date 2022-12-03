9ff116f67cc9aa1c0a3607d1695dcdba7f9f93e7
Rioni <nrosa01@ucm.es>
2022-12-03 18:21:14 +0100

    Añadido waypoints

--- Zona_Costera/Assets/Scripts/MoveCharactersAI.cs
+++ Zona_Costera/Assets/Scripts/MoveCharactersAI.cs
@@ -6,19 +6,29 @@ using UnityEngine.AI;
 public class MoveCharactersAI : MonoBehaviour
 {
     NavMeshAgent agent;
-    [SerializeField]Transform[] wayPoints;
+    [SerializeField] Transform[] wayPoints;
+    int currentIndex = 0;
 
     private int RandomIndex => Random.Range(0, wayPoints.Length);
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = wayPoints[RandomIndex].position;
+        currentIndex = RandomIndex;
+        agent.destination = wayPoints[currentIndex].position;
     }
 
     void Update()
     {
         if (agent.remainingDistance <= 0.1f)
-            agent.destination = wayPoints[RandomIndex].position;
+        {
+            int aux;
+            do
+            {
+                aux = RandomIndex;
+            } while (aux == currentIndex);
+            currentIndex = aux;
+            agent.destination = wayPoints[currentIndex].position;
+        }
     }
 }
