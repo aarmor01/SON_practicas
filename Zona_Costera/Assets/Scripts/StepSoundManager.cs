575ee038527a0d43615c7dc000c6b64c7b951344
Rioni <nrosa01@ucm.es>
2022-12-03 13:21:15 +0100

    StepSoundManager script

---
+++ Zona_Costera/Assets/Scripts/StepSoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class StepSoundManager : MonoBehaviour
+{
+    [SerializeField] EventReference footstepEventReference;
+    private EventInstance footsteps;
+
+    private void Awake()
+    {
+        if (footstepEventReference.IsNull)
+            footsteps = RuntimeManager.CreateInstance(footstepEventReference);
+    }
+
+    public void PlayFootsteps()
+    {
+        if (footsteps.isValid())
+            footsteps.start();
+    }
+}
