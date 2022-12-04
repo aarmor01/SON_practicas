d4d3a03064cd4e2a5406840ce3d679e7e90164aa
Rioni <nrosa01@ucm.es>
2022-12-04 20:14:43 +0100

    Cajas respawnean

---
+++ Zona_Costera/Assets/Scripts/SleepOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepOnCollision : MonoBehaviour
+{
+    [SerializeField] float restoreTime = 5.0f;
+
+    void RestoreRutine()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke(nameof(RestoreRutine), restoreTime);
+        gameObject.SetActive(false);
+    }
+}
