1b55b75b6f2e985912048b03fb1e69195f740450
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-12-04 20:59:27 +0100

    Comienzo viento

---
+++ Zona_Costera/Assets/Scripts/LowPassCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] LayerMask detectorLayers;
+    [SerializeField] Vector3 offset;
+
+    void Update()
+    {
+        //RaycastHit ray;
+        //var r = Physics.Raycast(transform.position, Vector3.down + offset, out ray, 40, detectorLayers);
+        //if (r)
+        //{
+        //    var collision = ray.collider.gameObject;
+        //    if (collision && collision != detected)
+        //    {
+        //        detected = collision;
+        //        surface = detected.GetComponent<SurfaceType>();
+        //        //if (surface)
+        //        //    Debug.Log("Detected " + detected.name + " with type: " + surface.SurfaceName);
+        //    }
+        //}
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawRay(transform.position + offset, Vector3.down);
+    }
+}
---
+++ Zona_Costera/Assets/Scripts/WindManager.cs
@@ -0,0 +1,37 @@
+﻿using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindManager : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private StudioEventEmitter windEvent;
+
+    const string parameter = "Height";
+    const float maxHeight = 10;
+
+    float ogHeight;
+
+    private void Awake()
+    {
+        windEvent = GetComponent<StudioEventEmitter>();
+        ogHeight = player.transform.position.y;
+        Debug.Log("Og Height: " + ogHeight);
+
+    }
+
+    private void Update()
+    {
+        float height = player.transform.position.y - ogHeight;
+
+        Debug.Log("Height: " + height);
+        height = Mathf.Max(height, 0);
+        height = Mathf.Min(height, maxHeight);
+        Debug.Log("Height: " + height);
+
+        windEvent.EventInstance.setParameterByName(parameter, height);
+        windEvent.EventInstance.start();
+
+    }
+}
