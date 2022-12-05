bde0b5ad94f8a3fdf73a33b2f5b8d180f22686ee
Rioni <nrosa01@ucm.es>
2022-12-05 17:19:27 +0100

    Particulas al romper las cajas

---
+++ Zona_Costera/Assets/Scripts/SpawnOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnCollision : MonoBehaviour
+{
+    [SerializeField]GameObject prefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
