4816bf587b2a477817a9c66900ba31bfb1ebdff1
Rioni <nrosa01@ucm.es>
2022-12-06 14:53:41 +0100

    Sonido de viento arreglado

--- Zona_Costera/Assets/Scripts/WindManager.cs
+++ Zona_Costera/Assets/Scripts/WindManager.cs
@@ -7,35 +7,26 @@ using UnityEngine.InputSystem.XR;
 
 public class WindManager : MonoBehaviour
 {
-    [SerializeField] EventReference windEvent;
-    private EventInstance windInstance;
+    [SerializeField] StudioEventEmitter windEvent;
+    [SerializeField] Transform character;
 
     const string parameter = "Height";
-    const float maxHeight = 10;
+    const float maxHeight = 12;
 
     float ogHeight;
 
     private void Awake()
     {
-        if (!windEvent.IsNull)
-            windInstance = RuntimeManager.CreateInstance(windEvent);
-
-        ogHeight = transform.position.y;
-        Debug.Log("Og Height: " + ogHeight);
-
+        ogHeight = transform.transform.position.y;
     }
 
     private void Update()
     {
-        float height = transform.position.y - ogHeight;
+        float height = character.position.y - ogHeight;
 
         height = Mathf.Max(height, 0);
         height = Mathf.Min(height, maxHeight);
 
-        if (windInstance.isValid())
-        {
-            windInstance.setParameterByName(parameter, 5f);
-            windInstance.start();
-        }
+        windEvent.EventInstance.setParameterByName(parameter, height);
     }
 }
